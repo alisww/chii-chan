1a9a238e3514e918d774b3be5ecfb810e8c48fd3
alisw <waylandalis@gmail.com>
2021-02-21 21:45:31 -0300

    fixed tw help

--- chiichan.py
+++ chiichan.py
@@ -500,10 +500,10 @@ async def help(ctx,*args):
         """
     elif 'tw' in args[0]:
         h = f"""
-        **{ctx.prefix}tw:show [manga name | mangaupdates link]**
+        **{ctx.prefix}tw show [manga name | mangaupdates link]**
         see trigger warnings submitted for this manga by other users. (p.s: the triggers are spoilered. you can also use this command in a dm with this bot.)
 
-        **{ctx.prefix}tw:add [manga name | mangaupdates link]**
+        **{ctx.prefix}tw add [manga name | mangaupdates link]**
         submit trigger warnings for this manga. (p.s: you can also use this command in a dm with this bot.)
 
         *ex: {ctx.prefix}tw show Still Sick*
